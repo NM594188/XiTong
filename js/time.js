81d92f7b23c0457fc49ab6e9740bc973c72c2900
Finger Travellers <119613758+NM594188@users.noreply.github.com>
2024-04-16 21:06:03 +0800

    time.js

--- js/time.js
+++ js/time.js
@@ -1,26 +1,22 @@
-
 todayDate = new Date();
 date = todayDate.getDate();
-month= todayDate.getMonth() +1;
-year= todayDate.getYear();
-document.write(" ")
-if(navigator.appName == "Netscape")
-{
-document.write(1900+year);
-document.write("Äê");
-document.write(month);
-document.write("ÔÂ");
-document.write(date);
-document.write("ÈÕ");
-}
-if(navigator.appVersion.indexOf("MSIE") != -1)
-{
-document.write(year);
-document.write("Äê");
-document.write(month);
-document.write("ÔÂ");
-document.write(date);
-document.write("ÈÕ");
+month = todayDate.getMonth() + 1;
+year = todayDate.getFullYear();
 document.write(" ");
+if (navigator.appName == "Netscape") {
+    document.write(year);
+    document.write("年");
+    document.write(month);
+    document.write("月");
+    document.write(date);
+    document.write("日");
 }
-
+if (navigator.appVersion.indexOf("MSIE") != -1) {
+    document.write(year);
+    document.write("年");
+    document.write(month);
+    document.write("月");
+    document.write(date);
+    document.write("日");
+    document.write(" ");
+}
